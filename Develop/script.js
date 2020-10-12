955f4ee54ceacccb4b50d1c9d1e0bd46abca3230
Alex Kahler <kahler197@gmail.com>
2020-10-12 17:41:27 -0500

    defined password parameters

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,13 @@ var confirmSpecialCharacter
 var confirmNumbers
 var confirmUpperCase
 var confirmLowerCase
+var password
+var passwordText
+
+number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "W", "Z"];
+confirmSpecialCharacter = ["!", "?", "#", "$", "%", "^", "*", "-", "_", "+"];
 
 var link = document.getElementById(generate)
 
@@ -28,9 +35,14 @@ if (confirmLowerCase === true|| confirmUpperCase === true) {
     alert("Please choose uppercase characters, lowercase characters or both.");
   }
 
-
-
 function writePassword() {
+
+  // logic arrays
+
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
